20f3d8d5d5a6e61e80ca96aff3024e2adcfe0996
Andres F. Virgüez <92434766+andresvBSE@users.noreply.github.com>
2024-08-02 18:18:11 +0000

    inclusion of highlights texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+
+sns.set_theme(style="whitegrid", palette="pastel")
+
 import matplotlib.pyplot as plt
 from pathlib import Path
 
@@ -12,22 +15,39 @@ def load_data():
     
     df = df.query("Store<6") # only 5 stores
     df["Date"] = pd.to_datetime(df['Date'], format="%d-%m-%Y")
+    df["Date S F"] = df['Date'].dt.strftime("%Y-%m-%d")
+    df.sort_values(by="Date", inplace=True)
     return df
 data = load_data()
 
+@st.cache
+def load_highlights_data():
+    DATA_FILENAME = Path(__file__).parent/'data/highlights.csv'
+    df = pd.read_csv(DATA_FILENAME)
+    return df
+data_hihglights = load_highlights_data()
+
+
+
 # Helper functions to calculate metrics
 def get_total_sales(data, week):
-    return data[data['Date'].dt.isocalendar().week == week]['Weekly_Sales'].sum()
+    return data[data['Date S F'] == week]['Weekly_Sales'].sum()
 
 def get_weekly_sales(data, week):
-    return data[data['Date'].dt.isocalendar().week == week].groupby('Store')['Weekly_Sales'].sum()
+    return data[data['Date S F'] == week].groupby('Store')['Weekly_Sales'].sum()
 
 def calculate_sales_variation(current_sales, last_sales):
     return ((current_sales - last_sales) / last_sales) * 100 if last_sales != 0 else 0
 
+def get_weekly_highlights(data, week):
+    #try:
+        return data[data["Week"] == week]["Highlights"][0]
+    #except:
+    #    return "No data"
+    
+
 # Get the list of available weeks
-available_weeks = data['Date'].dt.isocalendar().week.unique()
-available_weeks = sorted(available_weeks)
+available_weeks = [week for week in data["Date S F"].unique()][1:]#since the second week
 
 # Streamlit app layout
 st.title("Walmart Sales Dashboard")
@@ -36,7 +56,9 @@ st.title("Walmart Sales Dashboard")
 selected_week = st.selectbox("Select Week", available_weeks)
 
 # Calculate metrics for the selected week and the previous week
-last_week = selected_week - 1 if selected_week - 1 in available_weeks else selected_week
+
+last_week_index = available_weeks.index(selected_week) - 1 if available_weeks.index(selected_week) - 1 >= 0 else 0
+last_week =  available_weeks[last_week_index]
 
 total_sales_current_week = get_total_sales(data, selected_week)
 total_sales_last_week = get_total_sales(data, last_week)
@@ -77,5 +99,14 @@ plt.ylabel('Sales')
 st.pyplot(plt)
 
 # Sales improvement text
-improvement_text = f"Sales {'improved' if sales_variation > 0 else 'declined'} by {abs(sales_variation):.2f}% compared to last week."
-st.text(improvement_text)
+#improvement_text = f"Sales {'improved' if sales_variation > 0 else 'declined'} by {abs(sales_variation):.2f}% compared to last week."
+
+
+print(selected_week)
+
+if get_weekly_highlights(data_hihglights, selected_week):
+    highlights_text = get_weekly_highlights(data_hihglights, selected_week)
+else:
+    highlights_text = "No additional information available."
+
+st.markdown(highlights_text)
